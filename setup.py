c3beb459af0a8b096408f768098c33bcc3c1b38c
zhangmingming <690126048@qq.com>
2021-12-02 21:35:53 +0800

    0.0.5

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 #-*-coding:utf-8-*-
 from setuptools import setup, find_packages
-import os
-import shutil
-import platform
 
 with open("README.md","r",encoding="utf-8") as f:
     desc = f.read()
 
-system = platform.system()
-if system == 'Linux':
-    path = '/usr/share/fonts/'
-    if not os.path.exists(path):
-        os.mkdir(path)
-    shutil.copy('gvcode/fonts/arial.ttf', path + 'arial.ttf')
 
 setup(
     name="gvcode",
-    version="0.0.4",
+    version="0.0.5",
     description="A useful verification code generation tool.",
     long_description=desc,
     long_description_content_type="text/markdown",
